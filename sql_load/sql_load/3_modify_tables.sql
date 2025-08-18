62bc4ae61990801ede530cde792d3473c5063efb
GiwrgosTsitsos <giwrgostsitsos@gmail.com>
2025-08-18 19:23:17 +0300

    first commit

---
+++ sql_load/sql_load/3_modify_tables.sql
@@ -0,0 +1,33 @@
+COPY company_dim
+FROM 'C:\Users\30698\Documents\DataNerd\SQL_Project_Data_Job_Analysis\csv_files-SQLTutorialLuke1\csv_files\company_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY skills_dim
+FROM 'C:\Users\30698\Documents\DataNerd\SQL_Project_Data_Job_Analysis\csv_files-SQLTutorialLuke1\csv_files\skills_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY job_postings_fact
+FROM 'C:\Users\30698\Documents\DataNerd\SQL_Project_Data_Job_Analysis\csv_files-SQLTutorialLuke1\csv_files\job_postings_fact.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+COPY skills_job_dim
+FROM 'C:\Users\30698\Documents\DataNerd\SQL_Project_Data_Job_Analysis\csv_files-SQLTutorialLuke1\csv_files\skills_job_dim.csv' WITH (
+        FORMAT csv,
+        HEADER true,
+        DELIMITER ',',
+        ENCODING 'UTF8'
+    );
+SELECT *
+FROM company_dim
+SELECT *
+FROM company_dim
+LIMIT 100;
